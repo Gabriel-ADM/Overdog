30bd87e8912fed7ac9ef51960acd54f9f2eca995
Gabriel de Oliveira <gabriel.admpinho@gmail.com>
2021-05-07 18:17:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var lsProduto = getLsProdutos();
+
 function addProduto(i, qt) {
     produto = lsProduto[i];
     produto.qt += qt;
-    (produto.qt < 0) ? produto.qt = 0 : "";
+    (produto.qt < 0) ? produto.qt = 0: "";
     document.getElementById("cod-" + i).innerHTML = `R$ ${(produto.valor * produto.qt).toFixed(1)} (x${produto.qt})`;
 }
 
 function criar() {
-    g = "";
+    gr = "";
     p = lsProduto[1];
     for (i in lsProduto) {
         p = lsProduto[i];
 
-        if (g != p.grupo) {
+        if (gr != p.grupo) {
             var grupo = document.createElement("div");
             grupo.classList.add("grupo");
             grupo.innerHTML = `<h4>${p.grupo}<span>${p.tipo}</span></h4>`;
             document.getElementById("conteudo").appendChild(grupo);
-            g = p.grupo;
+            gr = p.grupo;
         }
 
         if (p.cod != '') {
@@ -67,7 +68,7 @@ function enviar() {
     for (i in lsProduto) {
         p = lsProduto[i]
         if (p.qt > 0) {
-            (p.cod.length > 3) ? extrato += '' : extrato += '№ ';
+            (p.cod.length > 3) ? extrato += '': extrato += '№ ';
             extrato += `${p.cod} (${p.qt}x R$${p.valor.toFixed(1)}) = R$${(p.qt * p.valor).toFixed(1)} <br>`;
             total += p.qt * p.valor;
         }
@@ -75,11 +76,11 @@ function enviar() {
     if (extrato != "") {
         extrato += `TOTAL PRODUTOS = R$${total.toFixed(1)}`;
         document.getElementById("complemeto").style.display = 'block';
-        document.getElementById("bt-enviarws").style.display = 'block';
+        document.getElementById("bt-enviarwpp").style.display = 'block';
     } else {
         extrato = 'Necessário escolhor ao menos 1 produto.';
         document.getElementById("complemeto").style.display = 'none';
-        document.getElementById("bt-enviarws").style.display = 'none';
+        document.getElementById("bt-enviarwpp").style.display = 'none';
     }
 
     document.getElementById("pedido").innerHTML = extrato;
@@ -87,18 +88,21 @@ function enviar() {
 
 }
 
-function enviarWs() {
-    fone = '5561994370463';
+function enviarWpp() {
+    fone = '5561981733270';
     nome = document.getElementById("nome").value;
     endereco = document.getElementById("endereco").value;
-    (endereco != '') ? pedido += `<br>endereco: ${endereco}`:'';
-    if (nome == '') {alert("Digite seu nome"); return ;}
+    (endereco != '') ? pedido += `<br>Endereco: ${endereco}`: '';
+    if (nome == '') {
+        alert("Digite seu nome");
+        return;
+    }
     pedido += `<br>Nome: ${nome}`;
     pedido = pedido.replaceAll('<br>', '\n');
     pedido = encodeURI(pedido);
     console.log(pedido)
     link = `https://api.whatsapp.com/send?phone=${fone}&text=${pedido}`;
-    window.open(link,'_blank');
+    window.open(link, '_blank');
 }
 criar();
 var acc = document.getElementsByClassName("item");
@@ -130,7 +134,6 @@ btnX.onclick = function () {
 }
 
 window.onclick = function (event) {
-    
     if (event.target == modal) {
         modal.style.display = "none";
     }
